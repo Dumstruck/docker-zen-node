5c2890a89248b557a1ce5ef68a3d846853bad7db
Jonathan Dumaine <jonathan.dumaine@dumstruck.com>
2017-11-27 16:17:56 -0700

    Allow entry point command to be overridden

--- secnodetracker/entrypoint.sh
+++ secnodetracker/entrypoint.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 # link the secure node tracker config, bail if not present
-if [ -f "/mnt/zen/secnode/stakeaddr" ]; then
-  echo "Secure node config found OK - linking..."
-  ln -s /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
+#if [ -f "/mnt/zen/secnode/stakeaddr" ]; then
+  #echo "Secure node config found OK - linking..."
+  #ln -s /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
+#else
+  #echo "No secure node config found. exiting"
+  #exit 1
+#fi
+
+cd /home/node/secnodetracker
+
+if [ -f ./config/stakeaddr ]; then
+  echo "Secure node config found OK"
 else
-  echo "No secure node config found. exiting"
+  echo "No secure node config found. Exiting"
   exit 1
 fi
 
-# Copy the zencash params
-cp -r /mnt/zen/zcash-params /mnt/zcash-params
+# Fix permissions of config files
+chown -R node:node ./config
 
-# Fix the permissons
-chown -R node:node /home/node/secnodetracker/config /mnt/zcash-params
-
-cd /home/node/secnodetracker
-gosu node node app.js
+su-exec node "$@"
